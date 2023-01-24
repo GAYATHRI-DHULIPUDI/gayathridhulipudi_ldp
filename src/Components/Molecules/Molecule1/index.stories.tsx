3c82759a52a599b970ce01d52c1de24992d52189
GAYATHRI-DHULIPUDI <119929520+GAYATHRI-DHULIPUDI@users.noreply.github.com>
2023-01-24 14:17:59 +0530

    Delete src directory

--- src/Components/Molecules/Molecule1/index.stories.tsx
+++
@@ -1,8 +0,0 @@
-import MuiGrid from '.';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-export default{
-    title:"Molecule/Grid1",
-    component:MuiGrid
-} as ComponentMeta<typeof MuiGrid>;
-const Template: ComponentStory<typeof MuiGrid>= () => <MuiGrid/>
-export const Primary = Template.bind({});
--- src/Components/Molecules/Molecule2/index.stories.tsx
+++
@@ -1,8 +0,0 @@
-import MuiGrid2 from '.';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-export default{
-    title:"Molecule/Grid2",
-    component:MuiGrid2
-} as ComponentMeta<typeof MuiGrid2>;
-const Template: ComponentStory<typeof MuiGrid2>= () => <MuiGrid2/>
-export const Primary = Template.bind({});
